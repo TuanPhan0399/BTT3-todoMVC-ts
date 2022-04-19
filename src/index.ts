04375dd895e32df7de3b12b1ed512c14e30592c8
TuanPhan0399 <tt.tuan.phan@tomosia.com>
2022-04-19 15:16:14 +0700

    show code

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,52 @@
 // Selector
 const taskInput = <HTMLInputElement>document.querySelector(".task-input input");
+const taskBox = <HTMLUListElement>document.querySelector(".task-box");
 
 // getting localstorage todo-list
-let todos = JSON.parse(localStorage.getItem("todo-list") || "[]");
+let todos: [
+  {
+    taskValue: string;
+    taskStatus: string;
+  }
+] = JSON.parse(localStorage.getItem("todo-list") || "[]");
 let taskInfo: {
   taskValue: string;
   taskStatus: string;
 };
 
-taskInput.addEventListener("keyup", (e) => {
+// Event Listener
+taskInput.addEventListener("keyup", saveTask);
+
+//function
+
+function saveTask(e: KeyboardEvent) {
+  // Do not take spaces and null characters
   let userTask = taskInput.value.trim();
   if (e.key === "Enter" && userTask) {
     taskInput.value = "";
+    // Add one task new on todos
     taskInfo = {
       taskValue: userTask,
       taskStatus: "pending",
     };
     todos.push(taskInfo);
     localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo();
   }
-});
+}
+function showTodo() {
+  let li = "";
+  todos.forEach((todo, id) => {
+    li += `<li class="task">
+            <div>
+              <input onclick="updateStatus(this)" type="checkbox" id="${id}">
+              <span ondblclick="editTask(this)" class="${id}">${todo.taskValue}</span>
+            </div>
+            <div class="task-close">
+              <i onclick="deleteTask(${id})" class="fa-solid fa-xmark"></i>
+            </div>
+          </li>`;
+  });
+  taskBox.innerHTML = li;
+}
+showTodo();
