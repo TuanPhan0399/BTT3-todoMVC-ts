e7aa7bca8fd3e122fd422dfae91e1aeb68a25183
TuanPhan0399 <tt.tuan.phan@tomosia.com>
2022-04-19 15:18:47 +0700

    update

--- public/bundle.js
+++ public/bundle.js
@@ -22,12 +22,12 @@ eval("\n// Selector\nconst taskInput = document.querySelector(\".task-input inpu
 
 /******/ 	});
 /************************************************************************/
-/******/ 	
+/******/
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/index.ts"]();
-/******/ 	
+/******/
 /******/ })()
 ;
